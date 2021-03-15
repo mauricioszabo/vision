38994e523303913fee817dd4ea361741e6a9d0c9
Maurício Szabo <mauricio.szabo@gmail.com>
2021-03-15 10:50:05 -0300

    Vega code being rendered on client-side

--- test/vision/core_test.cljs
+++ test/vision/core_test.cljs
@@ -1,25 +1,21 @@
 (ns vision.core-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.test :refer [deftest]]
             [vision.web :as web]
-            ; [vision.node :as node]
             [promesa.core :as p]
-            [check.async :refer [check testing async-test]]
+            [check.async :refer [check async-test]]
             [shadow.resource :as resource]))
 
 (def edn-vega
-  {:$schema "https://vega.github.io/schema/vega-lite/v4.json",
+  {:$schema "https://vega.github.io/schema/vega-lite/v4.json"
    :width "container"
-   :height 250,
+   :height 250
    :data {:values [{"Origin" "USA"}
                    {"Origin" "USA"}
-                   {"Origin" "Japan"}]
-          :mark "bar"
-          :encoding {:x {:field "Origin"},
-                     :y {:aggregate "count",
-                         :title "Number of Cars"}}}})
-
-(def vega-code
-  (resource/inline "vega_test.cljs"))
+                   {"Origin" "Japan"}]}
+   :mark "bar"
+   :encoding {:x {:field "Origin"}
+              :y {:aggregate "count"
+                  :title "Number of Cars"}}})
 
 (defn await-for [fun]
   (p/loop [n 0]
@@ -31,17 +27,13 @@
 
 (defn- prepare-fixture-div! []
   (p/let [root (await-for #(js/document.querySelector "#test-root"))
-          div (doto (js/document.createElement "div")
+          div (or (js/document.querySelector "div#test-fixture")
+                  (js/document.createElement "div"))
+          div (doto div
                     (aset "id" "test-fixture")
                     (.. -classList (add "container")))]
     (.appendChild root div)
     div))
-
-; (def simple-code
-;   '(let [root (dom/query "#test-root")
-;          container (doto (dom/create "div")
-;                          (dom/set-text "Hello, world"))]
-;      [:div ]))
 
 (defn- await-for-text [text]
   (await-for #(let [doc (js/document.querySelector "#test-fixture")
@@ -56,46 +48,25 @@
       (web/render "#test-fixture" code)
       (check (await-for-text "Hello, world") => "Hello, world"))))
 
-(def simple-code
-  (str '(def some-text (js/require "some-lib"))
-       "\n\n"
-       '[:div some-text]))
-
-#_
-(deftest bunding-simple-packages
-  (async-test "Bundles a code and renders the hiccup" {:timeout 6000}
+(deftest rendering-cljs-functions
+  (async-test "Renders based on a ClojureScript function" {:timeout 6000}
     (prepare-fixture-div!)
-    (p/let [bundled (node/bundle-cljs simple-code
-                                      {:input-fs {:stat (fn [a callback]
-                                                          (prn :STAT a)
-                                                          (cond
-                                                            (re-find #"node_modules" a)
-                                                            (callback nil
-                                                             #js {:isFile (constantly false)
-                                                                  :isDirectory (constantly true)})
+    (let [code (pr-str '(fn [person]
+                          (let [div (dom/create "div")]
+                            (dom/add-class! div ["container"])
+                            (dom/set-text! div (str "Hello, " person))
+                            div)))
+          component (-> code web/evaluate! web/to-reagent)]
+      (web/render "#test-fixture" [component "Ariovaldo"])
+      (check (await-for-text "Hello, Ariovaldo") => "Hello, Ariovaldo"))))
 
-                                                            (re-find #"some-lib\.js" a)
-                                                            (callback nil
-                                                                      #js {:isFile (constantly true)
-                                                                           :isDirectory (constantly false)})))
-                                                  ;:readlink
-                                                  :readdir (fn [_ callback]
-                                                             (prn :READDIR)
-                                                             (callback #js ["some-lib.js"]))
-                                                  :readFile (fn [_ callback]
-                                                              (prn :READFILE _)
-                                                              (callback "Hello, Bundled"))}})]
-      (prn :B bundled))))
-      ;     code (pr-str '(let [code "Hello, world"]
-      ;                     [:div code]))]
-      ; (web/render "#test-fixture" code)
-      ; (check (await-for-text "Hello, world") => "Hello, world"))))
+(def vega-code
+  (resource/inline "vega-bundled.cljs"))
 
-
-#_
-(deftest bundler-for-js-objects
-  (async-test "Lol, wow"
-    (let [fixture (prepare-fixture-div!)]
-      (p/do!
-       (p/delay 1000)
-       (check (p/delay 200 {:foo 1}) => {:foo 20})))))
+(deftest rendering-vega-bundled-visualizer
+  (async-test "Renders a hiccup tag" {:timeout 6000}
+    (prepare-fixture-div!)
+    (let [component (-> vega-code (str "\nvega-tag") web/evaluate! web/to-reagent)]
+      (web/render "#test-fixture" [component edn-vega])
+      (check (await-for #(js/document.querySelector "#test-fixture div.chart-wrapper"))
+             => some?))))
