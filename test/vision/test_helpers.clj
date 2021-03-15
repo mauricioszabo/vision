e6fd2c5df611ef271040c245e7081386069a2410
Maurício Szabo <mauricio.szabo@gmail.com>
2021-03-15 10:49:42 -0300

    Don't know if this will work...

--- test/vision/test_helpers.clj
+++ test/vision/test_helpers.clj
@@ -1,9 +1,9 @@
-(ns vision.test-helpers
-  (:require [clojure.edn :as edn]))
+(ns vision.test-helpers)
+  ; (:require [clojure.edn :as edn]))
 
 (defn http [req]
-  (def req req)
-  (def body (-> req :body slurp edn/read-string))
+  ; (def req req)
+  ; (def body (-> req :body slurp edn/read-string))
   ; (def a (-> req :body  .readAllBytes))
   ; (prn (-> req :body  .readAllBytes String.))
   {:status 200
